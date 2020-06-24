5c26f0f070e143da569f2844893a5044166e7f1b
skdcodes <skdcodes@gmail.com>
2020-06-25 02:01:14 +0530

    Minimze resources

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 - Changing height puts texts from different regions
 - Fixing 1px margin at top http://localhost:1234/#customers
 - How to make better responsive images in flex box?
-- Minimize resources
+
+- Change subtitle text to more condensed
+- Make the logo bigger, the subtitle smaller
--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,25 @@
 "use strict";
 
+// Animate on scroll dependencies
+import AOS from "aos";
+import "aos/dist/aos.css";
 
+// Initialize AOS
+AOS.init({
+  duration: 1000,
+});
 
-$(document).ready(function () {
-    AOS.init({
-        duration: 1000,
-    });
-    
-  // Add navbar background
-  $(window).scroll(function () {
-    var position = $(document).scrollTop();
-    if (position > 0) {
-      $(".navbar").addClass("has-bg");
-    } else {
-      $(".navbar").removeClass("has-bg");
-    }
-  });
+// Bootstrap dependencies
+import $ from "jquery";
+import "bootstrap/js/src/collapse.js";
+import "bootstrap/js/src/scrollspy.js";
+
+// Toggle Navbar background
+$(window).scroll(function () {
+  const position = $(document).scrollTop();
+  if (position > 0) {
+    $(".navbar").addClass("has-bg");
+  } else {
+    $(".navbar").removeClass("has-bg");
+  }
 });
