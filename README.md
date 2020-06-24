d8b83d121c819a8d1b55478610ab872f1bc23cfb
skdcodes <skdcodes@gmail.com>
2020-06-24 23:44:14 +0530

    Include animations and Menu

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# TODO
+# Enhancements
 
 - Changing height puts texts from different regions
 - Fixing 1px margin at top http://localhost:1234/#customers
+- How to make better responsive images in flex box?
+- Minimize resources
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,19 @@
 "use strict";
 
-console.log("test");
+
+
+$(document).ready(function () {
+    AOS.init({
+        duration: 1000,
+    });
+    
+  // Add navbar background
+  $(window).scroll(function () {
+    var position = $(document).scrollTop();
+    if (position > 0) {
+      $(".navbar").addClass("has-bg");
+    } else {
+      $(".navbar").removeClass("has-bg");
+    }
+  });
+});
